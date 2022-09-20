3341f7ad7d0728001688f19ff8924e0763c498c0
mwikalikyalo <winniemwikali07@gmail.com>
2022-09-20 12:04:06 +0300

    Add the initial menu list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h3>Restaurant Web App</h3>
-               
+          <div className="menu">
+            <ul className="list-group">
+              <li className="list-group-item">
+                <input className="form-check-input me-1" type="checkbox" value="" aria-label="..." />
+                Salad
+              </li>
+              <li className="list-group-item">
+                <input className="form-check-input me-1" type="checkbox" value="" aria-label="..." />
+                Entree
+              </li>
+              <li className="list-group-item">
+                <input className="form-check-input me-1" type="checkbox" value="" aria-label="..." />
+                Soup
+              </li>
+            </ul>
+          </div>
+                    
       </header>
     </div>
   );
